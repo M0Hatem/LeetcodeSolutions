d2fb9e8e4d69081dc9ab5ddad2144baaa78f06bf
Mohamed <moh.medh.tem1@gmail.com>
2024-05-08 21:06:43 +0300

    solved 1.1v(with whileLoop)

--- 1929. Concatenation of Array.ts
+++ 1929. Concatenation of Array.ts
@@ -1,3 +1,12 @@
 function getConcatenation(nums: number[]): number[] {
-    return [...nums, ...nums]
+    const ans:number[] = Array(nums.length*2)
+    let i = 0;
+    while(i<nums.length){
+        ans[i]=nums[i]
+        ans[nums.length+i]=nums[i]
+        i++
+    }
+    return ans
 }
+
+console.log(getConcatenation([1,2,3]))
