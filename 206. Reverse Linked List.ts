c79f4efddad1dbfcb09d0d090783628fbdb9f0cc
Mohamed <moh.medh.tem1@gmail.com>
2024-05-12 17:54:01 +0300

    solved 1.0v(recursive)

--- 206. Reverse Linked List.ts
+++ 206. Reverse Linked List.ts
@@ -10,13 +10,15 @@ class ListNode {
 
 
 function reverseList(head: ListNode | null): ListNode | null {
-    let prv: ListNode = null, current: ListNode = head;
-    while (current){
-        let next = current.next
-        current.next=prv
-        prv = current
-        current = next
-    }
-    return prv
+    if (head === null || head.next === null) return head;
+    let newHead = reverseList(head.next);
+    let front = head.next;
+    front.next = head;
+    head.next = null;
+    return newHead
 }
-
+const head = new ListNode(1,new ListNode(2,new ListNode(3,null)))
+const result = reverseList(head)
+if (result !== null) {
+    console.log(result);
+}
